eaa43fadf0d27b8047fc5738b40d6b3680f2ad07
yosh8 <y.yahata@gmail.com>
2010-05-20 04:32:33 +0900

    一応できた

--- hard.pl
+++ hard.pl
@@ -3,29 +3,70 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+my @sches = ();
+my @countOfMap = ();
+
 &main();
 exit(0);
 
 sub main {
     my @lines = <STDIN>;
-    my @sches = ();
+    # 入力を２次元配列に。
     foreach my $line (@lines) {
         chomp($line);
-        my @days = split(//, $line);
+        my @days = split(/ /, $line);
         push(@sches, \@days);
     }
-    warn Dumper @sches;
+    warn Dumper \@sches;
 #    warn Dumper $#{$sches[0]}+1;
-    my @ownerOfMap = &initOwner($#sches + 1);
-    warn Dumper @ownerOfMap;
+    @countOfMap = &initOwner($#sches + 1);
+    warn Dumper \@countOfMap;
+    # 各人のスケジュールについて他の人のスケジュールに同じ日があるか見ていく
+    # 前提としてはi郎の方が空いてる日が少ない。ので他の人に渡したい。
+    for(my $i=0; $i <= $#sches; $i++) {
+        if(0 == $countOfMap[$i]) {
+            next;
+        }
+        for(my $j=$i+1; $j <= $#sches; $j++) {
+            my ($ret, $day) = &merge($i, $j);
+            print("i=$i, j=$j\n"); warn Dumper \@countOfMap;
+            # 地図を渡したら次の人に。
+            if(1 == $ret) {
+                print("schedule matched\n");
+                # j郎の地図の数が人数と同じなら全部集まった！
+                if($countOfMap[$j] == ($#sches+1)) {
+                    print("integrated at $day th!\n");
+                    return;
+                }
+                last;
+            }
+        }
+    }
 }
 
 sub initOwner {
     my $numberOfOwner = shift;
-    my @ownerOfMap = ();
+    my @countOfMap = ();
     for(my $i = 0; $i < $numberOfOwner; $i++) {
-        $ownerOfMap[$i] = $i;
+        $countOfMap[$i] = 1;
     }
-    return @ownerOfMap;
+    return @countOfMap;
 }
 
+sub merge {
+    my $i = shift;
+    my $j = shift;
+    for(my $indexOfDay=0;$indexOfDay <= $#{$sches[$j]}; $indexOfDay++) {
+        if($sches[$i][0] < $sches[$j][$indexOfDay]) {
+            return 0;
+        } elsif($sches[$i][0] > $sches[$j][$indexOfDay]) {
+            next;
+        } elsif($sches[$i][0] == $sches[$j][$indexOfDay]) {
+            # i郎の持っている地図をj郎に渡す
+            $countOfMap[$j] += $countOfMap[$i];
+            $countOfMap[$i] = 0;
+            return (1, $sches[$j][$indexOfDay]);
+        }
+    }
+    return 0;
+}
